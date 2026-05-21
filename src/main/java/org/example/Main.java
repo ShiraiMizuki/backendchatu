715b058f5b618c21055152b14c4310f40fc80210
ShiraiMizuki <shirai110409@gmail.com>
2026-05-21 18:20:13 +0200

    dodano wysylanie nazwy i daty wiadomosci

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,19 @@ import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static List<String> wiadomosci = new ArrayList<>();
+    private static List<Modelwiadomosci> wiadomosci = new ArrayList<>();
     public static void main(String[] args) {
                 int port = Integer.parseInt(System.getenv().getOrDefault("PORT" , "8080"));
                 Javalin backend = Javalin.create(config ->{
                     config.bundledPlugins.enableCors(cors -> cors.addRule(it -> it.anyHost()));
                 }).start(port);
-//wyslanie wiadomosci (post)
+// pobieranie wiadomosci(get (boze nwm jak ja mam to rozwinac dobrze))
         backend.get("/wiadomosci" , ctx->{
             ctx.json(wiadomosci);});
-// pobieranie wiadomosci(get (boze nwm jak ja mam to rozwinac dobrze))
+        //wyslanie wiadomosci (post)
         backend.post("/send" , ctx -> {
-            String tekst_android = ctx.body(); //pobranie tekstu z aplikacji
-            if (tekst_android!=null && !tekst_android.isBlank()){
+            Modelwiadomosci tekst_android = ctx.bodyAsClass(Modelwiadomosci.class); //pobranie tekstu z aplikacji
+            if (tekst_android!=null && !tekst_android.text.isBlank()){
                 wiadomosci.add(tekst_android);
                 System.out.println("nowa wiadomosc : " +tekst_android);
                 ctx.status(201).result("zapisane");
@@ -28,3 +28,17 @@ public class Main {
         });
         }
     }
+class Modelwiadomosci {
+    public String nazwa;
+    public String data;
+    public String text;
+    public Modelwiadomosci(){}
+    public Modelwiadomosci(String nazwa , String data , String tekst){
+        this.nazwa=nazwa;
+        this.data=data;
+        this.text=tekst;
+    }
+
+
+
+}
